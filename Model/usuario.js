97c2b5f9032622fef9a8e19e50581fd576a5b2fc
Daniel-Francisc <francisco.santos@undf.edu.br>
2025-06-28 17:54:28 -0300

    Create Connection

--- Model/usuario.js
+++
@@ -1,9 +0,0 @@
-const db = require('../config/Conexao');
-
-const Usuario = {
-  getAll: (callback) => {
-    db.query('SELECT * FROM tb_epi', callback);
-  }
-};
-
-module.exports = Usuario;
--- routes/Usuario.Routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controller/Controller');
-
-router.get('/Usuarios', UserController.listarUsuarios);
-
-module.exports = router;
