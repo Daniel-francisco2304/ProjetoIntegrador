7f298057af5bf1aa3db4c9ea7377a53486c5fb48
Daniel-Francisc <francisco.santos@undf.edu.br>
2025-10-05 13:53:05 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Home from './view/Home';
 import Login from './view/Login';
 import ResetScreen from './view/ResetPassword';
+import Funcionarios from './view/Funcionarios';
 import EpiScreen from './view/Epi';
 import Fornecedor from './view/Fornecedor';
 import Filial from './view/Filial';
@@ -8,14 +13,16 @@ import Acidente from './view/Acidante';
 function App() {
   return (
     <Router>
-      <Route path="/" element={<Login />} />
-      <Route path="/Home" element={<Home />} />
-      <Route path="/Acidente" element={<Acidente />} />
-      <Route path="/Epi" element={<EpiScreen />} />
-      <Route path="/Fornecedor" element={<Fornecedor />} />
-      <Route path="/Filial" element={<Filial />} />
-      <Route path="/ResetPassword" element={<ResetScreen />} />
-      <Route path="/funcionarios/*" element={<Funcionarios />} />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Acidente" element={<Acidente />} />
+        <Route path="/Epi" element={<EpiScreen />} />
+        <Route path="/Fornecedor" element={<Fornecedor />} />
+        <Route path="/Filial" element={<Filial />} />
+        <Route path="/ResetPassword" element={<ResetScreen />} />
+        <Route path="/funcionarios/*" element={<Funcionarios />} />
+      </Routes>
     </Router >
   );
 }
