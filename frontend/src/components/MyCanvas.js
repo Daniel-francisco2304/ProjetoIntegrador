2927585a0dcd4a45de420f10e977419fe274e073
Daniel-Francisc <francisco.santos@undf.edu.br>
2025-10-05 13:57:07 -0300

    Update MyCanvas.js

--- frontend/src/components/MyCanvas.js
+++ frontend/src/components/MyCanvas.js
@@ -31,7 +31,6 @@ export function MyCanva() {
                 </text>
             </div>
             <div>
-
                 <AnimatePresence>
                     {
                         isOpen && (
